15d2dfcb7a5eea8f3ab6484dbffe3caa5e89df5e
bfmcgo2 <https://bfmcgo2@github.com>
2019-07-15 23:49:26 -0400

    Cleaning up the onProgress issue

--- components/VideoPlayer/VideoPlayer.jsx
+++ components/VideoPlayer/VideoPlayer.jsx
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -6,34 +7,48 @@ import { Action } from 'redux-store/actions';
 import s from './VideoPlayer.scss';
 import locations from '../dummy_data/locations.json';
 
-function VideoPlayer(props){
-	if(!props.map) return <div></div>
-	return(
-		<ReactPlayer 
-			url={'https://www.youtube.com/watch?v=sBLvzupw6BI'} 
-			playing = { false } 
-			width='30%' 
-			height='30%'
-			controls= { true }
-			onProgress = { (e)=> {
-				let round_sec = Math.round(e.playedSeconds);
-				props.trackVideoProgress(round_sec);
-				console.log(round_sec);
-				locations.map(location => {
-					if(round_sec >= location.time_start && round_sec <= location.time_end) {
-						if(!props.location_active) {
-							props.toggleLocationActive()
-							props.map.flyTo({
-								center: location.coordinates,
-								zoom: 19
-							})
-						}
-					}
-				})
+class VideoPlayer extends Component {
+	state = {
+		location_active	: false,
+		location_id 	: 0
+	}
 
-			}}
-			className={s('VideoPlayer')}/>
-	)
+	render() {
+		if(!this.props.map) return <div></div>
+		return(
+			<ReactPlayer 
+				url={'https://www.youtube.com/watch?v=sBLvzupw6BI'} 
+				playing = { false } 
+				width='30%' 
+				height='30%'
+				controls= { true }
+				onProgress = { (e)=> {
+					let round_sec = Math.round(e.playedSeconds);
+					this.props.trackVideoProgress(round_sec);
+					console.log(this.state.location_id);
+					locations.map(location => {
+						if(round_sec >= location.time_start && round_sec < location.time_end) {
+							if(this.state.location_id !== location.id) {
+								this.setState({
+									location_id : location.id
+								})
+								this.props.map.flyTo({
+									center: location.coordinates,
+									zoom: 19
+								})
+							}
+							
+						}
+						
+						
+					})
+
+				}}
+				className={s('VideoPlayer')}/>
+		)
+	}
+
+	
 }
 
 
